2a603b244e14436555f8efe07c6785b946fa5ae5
Zorina Elena <38881469+TotoroDes@users.noreply.github.com>
2018-05-26 22:46:33 +0300

    Update proto.js

--- 26.05.18/proto.js
+++ 26.05.18/proto.js
@@ -1,1 +1,140 @@
-1
+var User = function(firstName, lastName, email, balance){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.balance = balance;
+    this.isValid = function(){
+        var isvalid = true;
+        if(this.firstName.trim().length === 0 || this.lastName.trim().length === 0){
+            isvalid = false;
+        }
+        if(!isFinite(+this.balance)){
+            isvalid = false;
+        }
+        if(this.email.indexOf('@') === -1 || this.email.trim().length === 0){
+            isvalid = false;
+        }
+        return isvalid;
+    };
+
+    this.valueOf = function(balance){
+        if(isFinite(Number(this.balance))){
+            return (Number(this.balance));
+        }
+    };
+}
+
+/*
+var userString = 'firstName=John, email=example@gmail.com, balance=300; firstName=Test, lastName=Test, email=admin@gmail.com, balance=1000';
+var regA = /=/g;
+var userString = userString.replace(regA,":");
+var userArray = userString.trim().split(';');
+var oneUser = userArray[0].split(',');
+var oneUser2 = userArray[1].split(',');
+var newUs = [];
+var newUs2 = [];
+var regB = /"/g;
+var str = oneUser2.join(',');
+for(var i = 0; i<oneUser.length; i++){
+  var res = oneUser[i].split(":", oneUser.length);
+  newUs.push(res[1].replace(regB,""));
+}
+for(var i = 0; i<oneUser2.length; i++){
+  var res2 = oneUser2[i].split(":", oneUser.length);
+  newUs2.push(res2[1].replace(regB,""));
+}
+var obg = new User('user','user','eser@user',300);
+*/
+
+var str = 'firstName=John, email=example@gmail.com, balance=300; firstName=Test, lastName=Test, email=admin@gmail.com, balance=1000';
+var objOne = str.split(';').map(function(userAtrributesString){
+    return userAtrributesString.split(',').reduce(function (result, keyValueString) {
+        var keyAndValue = keyValueString.split('=');
+        result[keyAndValue[0].trim()] = keyAndValue[1].trim();
+        return result;
+    }, new User);   // не успела сделать правильно((
+});
+
+var UserCollection = function() {
+    this.users = [];
+}
+
+UserCollection.prototype = {
+
+    add: function (payload) {
+        return this.users.push(payload)
+    };
+
+    remove: function (payload) {
+    for(var i=0; i<this.users.length; i++){
+        if(JSON.stringify(this.users[i]) === JSON.stringify(payload)){
+            this.users.splice(i,1);
+        }
+    }
+        return this.users;
+};
+
+    addAll: function (newUsersArray) {
+        this.users = this.users.concat(newUsersArray);
+        return this.users;
+};
+
+    clear: function () {
+    return this.users = [];
+};
+
+    findBy: function (propertyName, propertyValue) {
+    var resultArray = this.users.reduce(function (result, keyValueString) {
+
+        for(propertyName in keyValueString){
+            if(keyValueString[propertyName] === propertyValue){
+                result.push(keyValueString);
+            }
+        }
+        return result;
+
+    }, []);
+    return resultArray;
+};
+    sort:
+}
+
+
+var user2 = {
+    balance:'300',
+    email:"example@gmail.com",
+    firstName:"John",
+    lastName:"Die"
+
+}
+
+var user3 = {
+    balance:'200',
+    email:"example@gmail.com",
+    firstName:"John",
+    lastName:"Die"
+}
+
+
+userCollection = new UserCollection(objOne);
+
+userCollection.add(user2);
+userCollection.add(user3);
+userCollection.addAll(objOne); // не арбайтен, хотя вроде два массива
+
+userCollection.sortBy('balance','asc'); //не арбайтен вообще вот такой исфинит не арбайтен
+/*
+this.valueOf = function(){
+    if(isFinite(Number(this.attributes.balance))){
+      return Number(this.attributes.balance);
+    };
+
+    */
+
+// userCollection.remove(user3);
+//userCollection.clear(user3);
+
+console.log(userCollection);
+console.log(userCollection.findBy('balance','200'));
+console.log(objOne);
+
