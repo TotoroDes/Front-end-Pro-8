0b45fd44c3716f1ae84268d1b76d0207417f5b99
Zorina Elena <38881469+TotoroDes@users.noreply.github.com>
2018-05-28 15:49:41 +0300

    Add files via upload

---
+++ carousel/carousel.js
@@ -0,0 +1,41 @@
+var CarouselImage = function (rootElement) {
+    this.rootElement = rootElement;
+    this.item = this.rootElement.querySelector('#carousel');
+    this.itemsImg = [].slice.call(this.item.children);
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.width = 800;
+    this.currentPosition = 0;
+    this.currentSlide = 1;
+    this.itemsImgLenght = (this.itemsImg.length)*this.width;
+};
+
+CarouselImage.prototype.next = function () {
+    if(this.currentSlide > this.itemsImg.length){
+        this.currentSlide = 1;
+    }
+    this.currentPosition =  Math.min(this.currentSlide * this.width, this.itemsImgLenght-this.width);
+    this.item.style.right = this.currentPosition +'px';
+    this.currentSlide++;
+     // this.item.appendChild(this.itemsImg[this.currentItemId]);
+      };
+
+
+CarouselImage.prototype.prev = function () {
+    //   this.item.insertBefore(this.itemsImg[this.itemsImg.length], this.item.firstElementChild);
+    this.currentPosition = Math.max(this.currentPosition - this.width, 0);
+    this.item.style.right = this.currentPosition +'px';
+    this.currentSlide++;
+         };
+
+CarouselImage.prototype.delegateEvents = function () {
+    this.rootElement.querySelector('#arrowNext').addEventListener('click', this.next);
+    this.rootElement.querySelector('#arrowPrew').addEventListener('click', this.prev);
+    return this;
+};
+
+
+CarouselImage.prototype.render = function () {
+    this.delegateEvents();
+    return this;
+};
