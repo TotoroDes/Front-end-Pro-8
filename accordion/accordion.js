268f347a93c553de87d1d549ff2c6e8067283233
Zorina Elena <38881469+TotoroDes@users.noreply.github.com>
2018-06-01 12:16:08 +0300

    Update accordion.js

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -1,25 +1,28 @@
 var Accordion = function (rootElement) {
     this.rootElement = rootElement;
+    this.buttonItemClickOne = this.rootElement.querySelector('.accordion-click-one');
+    this.buttonItemClickTwo = this.rootElement.querySelector('.accordion-click-two');
+    this.buttonItemClickTree = this.rootElement.querySelector('.accordion-click-three');
     this.buttonItem = this.rootElement.getElementsByClassName('accordion-button');
     this.buttonItems = [].slice.call(this.buttonItem);
-    this.content = this.rootElement.getElementsByClassName('accordion-content');
-    this.contentItems = [].slice.call(this.content);
+    this.contentAcc = this.rootElement.getElementsByClassName('accordion-content');
+    this.contentAccItems = [].slice.call(this.contentAcc);
     this.currentTabId = 0;
     this.active = 'isActive';
     this.toggleAccordion = this.toggleAccordion.bind(this);
     this.handleClickAccordion = this.handleClickAccordion.bind(this);
- };
+};
 
 Accordion.prototype.handleClickAccordion = function (event) {
     this.currentTabId = this.buttonItems.indexOf(event.target);
     this.setActive(this.buttonItems);
-    this.setActive(this.contentItems);
+    this.setActive(this.contentAccItems);
 };
 
 Accordion.prototype.toggleAccordion = function(){
     this.buttonItems[this.currentTabId].classList.add(this.active);
-    this.contentItems[this.currentTabId].classList.add(this.active);
-    this.rootElement.addEventListener('click', this.handleClickAccordion);
+    this.contentAccItems[this.currentTabId].classList.add(this.active);
+
 };
 
 Object.defineProperty(Accordion.prototype, 'currentTabId', {
@@ -41,4 +44,18 @@ Accordion.prototype.setActive = function (itemsCollection) {
             item.classList.remove(this.active);
         }
     }.bind(this));
+};
+
+
+Accordion.prototype.delegateEvents = function () {
+    this.buttonItemClickOne.addEventListener('click', this.handleClickAccordion);
+    this.buttonItemClickTwo.addEventListener('click', this.handleClickAccordion);
+    this.buttonItemClickTree.addEventListener('click', this.handleClickAccordion);
+    return this;
+};
+
+
+Accordion.prototype.render = function () {
+    this.delegateEvents();
+    return this;
 };
