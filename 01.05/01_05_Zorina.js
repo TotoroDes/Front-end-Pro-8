ce100150f4d617ae8f0fcd84936a1dacb1af9900
Zorina Elena <38881469+TotoroDes@users.noreply.github.com>
2019-07-26 13:31:00 +0300

    Update 01_05_Zorina.js

--- 01.05/01_05_Zorina.js
+++ 01.05/01_05_Zorina.js
@@ -1,13 +1,5 @@
-
-
 var arr = [1,47,6,81,19,'66',5,14,53,45,67,56];
-
-
 console.log(arr);
-
-
-
-
 
 ///every 
 
@@ -19,22 +11,11 @@ if(typeof inputArray[i] !== "number"){
 result = false;
 break;
 	}
-
-	
-
 }
-
-
 return result;
-
 }
-
-
 
 console.log(everyArr(arr));
-
-
-
 
 ///some 
 
@@ -46,38 +27,21 @@ if(typeof inputArray[i] === "number"){
 result = true;
 break;
 	}
-
-	
-
 }
-
-
 return result;
-
 }
-
-
-
 
 console.log(someArr(arr));
-
 
 ///forEach 
 
 function forEachArr(inputArray){
-
 for (var i = 0; i < inputArray.length; i++){
 console.log(inputArray[i])
-	
-
+ }
 }
-
-}
-
 
 forEachArr(arr);
-
-
 
 //filter
 
@@ -87,21 +51,11 @@ for (var i = 0; i < inputArray.length; i++){
 if(inputArray[i] > inputArray[i+1]){
 
 result.push(inputArray[i]);
-
-	}
-
-	
+ }
 }
-
-
 return result;
-
 }
-
-
-
 console.log(filterArr(arr));
-
 
 //map
 
@@ -110,20 +64,11 @@ var result = [];
 for (var i = 0; i < inputArray.length-1; i++){
 var newArrElement = inputArray[i]+inputArray[i+1]
 result.push(newArrElement);
-
-
 	
 }
-
-
 return result;
-
 }
-
-
-
 console.log(mapArr(arr));
-
 
 //reduce
 
@@ -131,20 +76,15 @@ function reduceArr(inputArray){
 var result = [];
 for (var i = 0; i < inputArray.length-1; i++){
 
-
 if(inputArray[i]-inputArray[i+1] < 0){
 	result.push(inputArray[i]);
 }
 
-
-	
 }
 
 
 return result;
 
 }
-
-
 
 console.log(reduceArr(arr));
